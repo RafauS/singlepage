eb4cb4a7034008adbe4b8047e49c84cb8dfda575
RafauS <volfen27@gmail.com>
2020-05-29 12:59:50 +0200

    init project

---
+++ script.js
@@ -0,0 +1,13 @@
+const servicesItems = document.querySelectorAll(".services-navbar-item");
+let activeItem = servicesItems[0]
+
+servicesItems.forEach(item => {
+    item.addEventListener('click', getItemList);
+})
+
+function getItemList(event) {
+    activeItem.querySelector(".navbar-left-color").classList.toggle("active-service")
+    activeItem = event.target.parentNode
+    activeItem.querySelector(".navbar-left-color").classList.toggle("active-service")
+}
+
